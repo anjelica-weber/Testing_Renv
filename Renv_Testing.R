dce54f74138a842b706320d23b4f8b626657942b
Weber <anjelica.weber@mssm.edu>
2022-08-04 11:38:35 -0400

    adding in functions

--- Renv_Testing.R
+++ Renv_Testing.R
@@ -1,8 +1,10 @@
 
 # Libraries ---------------------------------------------------------------
+library(here)
 library(tidyverse)
 library(xlsx)
 
 # Calling Packages --------------------------------------------------------
+test_data <- read.xlsx2(paste0(here(), "/Test Data.xlsx"), sheetIndex = 1)
 
-
+test_data2 <- test_data %>% filter(Col.1 == "a")
